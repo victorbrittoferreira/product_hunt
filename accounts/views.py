e304359a903da3c84bf342832e92666c514ec42a
victorbrittoferreira <victorbrittoferreira@gmail.com>
2021-09-16 12:23:19 -0300

    log in/out n sign up - ok

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 from django.contrib import auth
 
 # Create your views here.
+
+# SIGN UP
+
 def signup(request):
     if request.method == 'POST':
         #POST the  info and wants an account now!
@@ -10,31 +13,42 @@ def signup(request):
 
             try:
                 user = User.objects.get(username=request.POST['username'])
-                return render(request, 'accounts/signup.html', {'error':'That username already exist'})
+                return render(request, 'accounts/signup.html',
+                    {'error':'That username already exist'})
             
             except User.DoesNotExist:
-                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(request.POST['username'],
+                    password=request.POST['password1'])
                 auth.login(request,user)
                 return redirect('home')
         
         else:
-            return render(request, 'accounts/signup.html', {'error':'Passwords must match'})
+            return render(request, 'accounts/signup.html', 
+                {'error':'Passwords must be equal'})
     
     else:
         # GET - enter
         return render(request, 'accounts/signup.html')
 
+#LOG IN
+
 
 def login(request):
     if request.method == 'POST':
-        user = auth.authenticate(username=request.POST['username'],password=request.POST['password'])
+        user = auth.authenticate(username=request.POST['username'], 
+            password=request.POST['password'])
         if user is not None:
             auth.login(request, user)
             return redirect('home')
         else:
-            return render(request, 'accounts/login.html',{'error':'username or password is incorrect.'})
+            return render(request, 'accounts/login.html',{'error':'username or password is incorrect'})
+
+
     else:
         return render(request, 'accounts/login.html')
+
+#LOG OUT
+
 
 def logout(request):
     if request.method == 'POST':
