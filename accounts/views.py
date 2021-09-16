1404bbf126de43684eebbe74ee1523dcaa509df8
victorbrittoferreira <victorbrittoferreira@gmail.com>
2021-09-16 11:19:49 -0300

    pass system update

--- accounts/views.py
+++ accounts/views.py
@@ -7,24 +7,36 @@ def signup(request):
     if request.method == 'POST':
         #POST the  info and wants an account now!
         if request.POST['password1'] == request.POST['password2']:
+
             try:
                 user = User.objects.get(username=request.POST['username'])
                 return render(request, 'accounts/signup.html', {'error':'That username already exist'})
+            
             except User.DoesNotExist:
                 user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
                 auth.login(request,user)
                 return redirect('home')
+        
         else:
             return render(request, 'accounts/signup.html', {'error':'Passwords must match'})
+    
     else:
         # GET - enter
         return render(request, 'accounts/signup.html')
 
+
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'],password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html',{'error':'username or password is incorrect.'})
+    else:
+        return render(request, 'accounts/login.html')
 
 def logout(request):
-    #TODO route to HP and run logout
-
-    #return render(request, 'accounts/home.html')
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
