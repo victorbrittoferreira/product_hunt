ee95f213c866aa682c9e86fef3c4463c4a601f91
victorbrittoferreira <victorbrittoferreira@gmail.com>
2021-09-16 06:11:49 -0300

    sign up, login, e logout...building up

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render, get_object_or_404
-
-#from .models import Account
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        #POST the  info and wants an account now!
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error':'That username already exist'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request,user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error':'Passwords must match'})
+    else:
+        # GET - enter
+        return render(request, 'accounts/signup.html')
 
 def login(request):
     return render(request, 'accounts/login.html')
